632615b4c0a99fe2ea210f0b25681f35b821a455
KotoSaori <64761992+KotoSaori@users.noreply.github.com>
2021-01-27 20:12:05 +0900

    Add files via upload

---
+++ test01.js
@@ -0,0 +1,25 @@
+// github
+
+var answer = prompt('数字を入力してください');
+if (answer == 2) {
+  alert('正解');
+} else {
+  alert('不正解');
+}
+
+
+// var answer = prompt('りんごとバナナのどちらが好きですか？');
+// if (answer == 'りんご') {
+//   alert('りんごは英語でappleです');
+// } else if (answer == 'バナナ') {
+//   alert('バナナは英語でbananaです');
+// } else {
+//   alert('りんご or バナナを入力してください');
+// }
+
+
+// function calc(x) {
+//   return x * 2;
+// }
+//
+// alert(calc(3));
